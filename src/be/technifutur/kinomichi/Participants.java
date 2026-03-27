94d91113c18133d3d235959e01a856d75e72f049
William <william.williamson.91@gmail.com>
2026-03-27 09:55:24 +0100

    creation des classes

---
+++ src/be/technifutur/kinomichi/Participants.java
@@ -0,0 +1,9 @@
+package be.technifutur.kinomichi;
+
+public class Participants {
+    public String nom;
+    public String prénom;
+    public int téléphone;
+    public String mail;
+    public String club;
+}
---
+++ src/be/technifutur/kinomichi/PlageHoraire.java
@@ -0,0 +1,4 @@
+package be.technifutur.kinomichi;
+
+public class PlageHoraire {
+}
